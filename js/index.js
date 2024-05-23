302d211235fc582e215a089ac4360fb630f0e2e2
Your Name <mukulay96@gma.com>
2024-05-23 17:52:40 +0300

    fork ts

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var name1 = "Nonna";
+let name2 = "Madina";
+const name3 = "Madina";
+name2 = "New Madina";
+const name4 = "New Sasha";
+//NUMBER
+const num1 = 12312312;
+console.log(num1);
+//BIGINT
+const bigint = 1231231231231231n;
+console.log(bigint);
+//STRING
+//const st1 = "Nonna"
+const str1 = "I'm a programmer";
+//const st2 = "Madina";
+const str2 = `he say "Hi!"`;
+const name5 = "Sasha";
+console.log(name5);
+console.log(str2);
+//-------BOOLEAN---------
+const bool1 = true;
+const bool2 = false;
+//--------NULL------
+const nullData = null;
+const userName = null;
+//------UNDEFINED------
+const undef = undefined;
+let undef2;
+//-----SYMBOL------
+const uniq = Symbol("id");
+cosole.log(uniq);
+//----OBJECT------
+const obj = {
+  name: "Шамиль",
+  city: "Moscow",
+  userName: null,
+};
+console.log(obj.userName);
+obj.userName = "USER1234";
+console.log(47 + 89);
+console.log(47 + "89");
